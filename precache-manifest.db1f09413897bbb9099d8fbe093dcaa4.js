d837a6a991fa6f046719ce95ab63746e3b52a78b
Charles-Thibault <charles.thibault.verrier@gmail.com>
2020-12-14 18:00:20 +0100

    Updates

---
+++ precache-manifest.db1f09413897bbb9099d8fbe093dcaa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b5213c8c63197775e0965be1a1b0386",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ed06d37a80b72361b36",
+    "url": "./static/css/main.b0be4a3a.chunk.css"
+  },
+  {
+    "revision": "f936fcf920f069033be3",
+    "url": "./static/js/2.c0b6be71.chunk.js"
+  },
+  {
+    "revision": "9c0cb33feaac9e0ffefa65a3e3414483",
+    "url": "./static/js/2.c0b6be71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed06d37a80b72361b36",
+    "url": "./static/js/main.5bba1d7f.chunk.js"
+  },
+  {
+    "revision": "b315889134b8cc925939",
+    "url": "./static/js/runtime-main.eaeafe15.js"
+  }
+]);
